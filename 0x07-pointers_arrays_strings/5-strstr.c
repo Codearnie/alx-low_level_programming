083a6a55e9e03e5d3772e26bc0106bf670b87c98
Earnie the Coder <ek.business@yahoo.com>
2024-01-08 00:37:40 -0800

    Task 6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - located a substring
+ * @haystack: string to be observed
+ * @needle: substring to be located
+ * Return: pointer to beginning of substring located or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
